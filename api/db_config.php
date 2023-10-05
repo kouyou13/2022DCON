c521a3363e3539e6f6edccf083a5f85f0f59a8ad
Koyo Kinoshita <58977709+kouyou13@users.noreply.github.com>
2023-10-05 19:30:54 +0900

    Update db_config.php

--- api/db_config.php
+++ api/db_config.php
@@ -1,6 +1,6 @@
 <?php
-  define('DB_DATABASE', 'LAA1293503-2022dcon');
-  define('DB_USERNAME', 'LAA1293503');
-  define('DB_PASSWORD', 'Karasugekitai999');
+  define('DB_DATABASE', 'database_name');
+  define('DB_USERNAME', 'username');
+  define('DB_PASSWORD', 'password');
   define('PDO_DSN', 'mysql:host='.'mysql154.phy.lolipop.lan'.';dbname='. DB_DATABASE . ';charset=utf8;');
 ?>
